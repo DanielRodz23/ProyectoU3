d9d54af2bd4a721daa7b08d1e774d59239190809
Daniel Andres Rodriguez Guerrero <201G0282@rcarbonifera.tecnm.mx>
2024-05-08 21:38:47 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoU3/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ProyectoU3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ProyectoU3/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ProyectoU3
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
