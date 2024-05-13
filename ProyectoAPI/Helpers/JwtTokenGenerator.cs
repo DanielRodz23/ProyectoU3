16c6ca3d1407e6843a0d2ea9633c8f1a766ccd21
Daniel Andres Rodriguez Guerrero <201G0282@rcarbonifera.tecnm.mx>
2024-05-13 04:10:02 -0600

    JwtRegisteredClaimNames

---
+++ ProyectoAPI/Helpers/JwtTokenGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace ProyectoAPI.Helpers
+{
+    public static class JwtTokenGenerator
+    {
+        public static string GetToken(string token)
+        {
+            List<Claim> claims = new List<Claim>();
+            claims.Add(new Claim(JwtRegisteredClaimNames.Sid, ""));
+            return "";
+        }
+    }
+}
