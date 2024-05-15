0835408b3f392f27e46fd212f4a7825f1d4245cc
Daniel Andres Rodriguez Guerrero <201g0282@rcarbonifera.tecnm.mx>
2024-05-15 17:31:12 -0600

    Pequeña adición

--- ProyectoAPI/Controllers/ActividadesController.cs
+++ ProyectoAPI/Controllers/ActividadesController.cs
@@ -28,8 +28,24 @@ namespace ProyectoAPI.Controllers
             var actividadesDTO = mapper.Map<IEnumerable<ActividadesDTO>>(actividades);
             return Ok(actividadesDTO);
         }
-        [HttpGet("GetMyBorrador")]
-        public async Task<IActionResult> GetMyBorrador(){
+        [HttpGet("GetActividadOrBorrador/{id}")]
+        public async Task<IActionResult> GetActividadOrBorrador(int id)
+        {
+            var context = HttpContext;
+            var idUsuario = User.Identities.SelectMany(x => x.Claims).FirstOrDefault(x => x.Type == "id");
+            if (idUsuario == null) return BadRequest();
+
+            var activ = actividadesRepository.Get(id);
+            if (activ == null) return NotFound();
+            if (activ.IdDepartamento!=int.Parse(idUsuario.Value))
+            {
+                return Forbid();
+            }
+            var actDto = mapper.Map<ActividadesDTO>(activ);
+            return Ok(actDto);
+        }
+        [HttpGet("GetMyBorradores")]
+        public async Task<IActionResult> GetMyBorradores(){
             var context = HttpContext;
             var idUsuario = User.Identities.SelectMany(x => x.Claims).FirstOrDefault(x => x.Type == "id");
             if (idUsuario == null) return BadRequest();
@@ -44,10 +60,42 @@ namespace ProyectoAPI.Controllers
         public async Task<IActionResult> PostActividad(ActividadesDTO actividadesDTO)
         {
             var context = HttpContext;
+            var claimTkn = User.Identities.SelectMany(x=>x.Claims).FirstOrDefault(x => x.Type=="id");
+            if (claimTkn==null) return BadRequest();
+            var id = int.Parse(claimTkn.Value);
             actividadesDTO.Id = 0;
+            actividadesDTO.IdDepartamento = id;
+            actividadesDTO.FechaCreacion = DateTime.UtcNow;
+            //TODO validar aqui
             var actividades = mapper.Map<Actividades>(actividadesDTO);
-
+            actividadesRepository.Insert(actividades);
             return Ok();
         }
+        [HttpPut]
+        public async Task<ActionResult> UpdateActividad(ActividadesDTO actividadesDTO)
+        {
+            //Teoricamente es para agregar una actividad que ya estaba como borrador
+            
+            var actividad = actividadesRepository.Get(actividadesDTO.Id);
+            if (actividad==null) return BadRequest();
+            
+            var context = HttpContext;
+            var claimtkn = User.Identities.SelectMany(x=>x.Claims).FirstOrDefault(x=>x.Type=="id");
+            if (claimtkn==null) return BadRequest();
+
+            actividad.Titulo = actividadesDTO.Titulo;
+            actividad.Descripcion = actividadesDTO.Descripcion;
+            actividad.FechaRealizacion = actividadesDTO.FechaRealizacion;
+            actividad.Estado = actividadesDTO.Estado;
+            if (actividad.Estado==2) actividad.FechaCreacion = DateTime.UtcNow;
+            else {actividad.FechaCreacion = actividadesDTO.FechaCreacion; }
+
+            //TODO validar
+            actividad.IdDepartamento = int.Parse(claimtkn.Value);
+            actividadesRepository.Update(actividad);
+            return Ok();
+            
+        }
+
     }
 }
---
+++ ProyectoAPI/Controllers/DepartamentosController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ProyectoAPI.Models.DTOs;
+using ProyectoAPI.Models.Entities;
+using ProyectoAPI.Repositories;
+
+namespace ProyectoAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class DepartamentosController(DepartamentosRepository departamentosRepository, IMapper mapper) : ControllerBase
+    {
+        private readonly DepartamentosRepository departamentosRepository = departamentosRepository;
+        private readonly IMapper mapper = mapper;
+
+        public async Task<IActionResult> PostDepartamento(DepartamentosDTO departamentoDTO)
+        {
+            //Validar que este usuario sea el Admin
+            #region ValidarAdmin
+            var context = HttpContext;
+            var claimid = User.Identities.SelectMany(x => x.Claims).FirstOrDefault(x => x.Type == "id");
+            if (claimid == null) return BadRequest();
+            var CurrentUser = departamentosRepository.Get(int.Parse(claimid.Value));
+            if (CurrentUser == null) return BadRequest();
+            if (CurrentUser.IdSuperior!=null) return Forbid();
+            #endregion
+            
+            //TODO validar
+            var newDep = mapper.Map<Departamentos> (departamentoDTO);
+            departamentosRepository.Insert(newDep);
+            return Ok();
+        }
+    }
+}
